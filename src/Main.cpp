82b22d6cc86e1e21af42d9b7f151476ac798c0b9
Rahul Chakraborty <rc645312@gmail.com>
2025-07-09 14:38:04 +0530

    feat(libraries): add core libraries (#1)
    * feat(libraries): add imgui, sfml and box2d libraries
    
    - patched imgui-SFML.cpp line 956 to support newer imgui version
    
    * feat(libraries): remove unused items from libraries
    
    * feat(libraries): add spdlog for logging
    
    * feat(build): configure static linking and logging setup
    
    - set static build options for sfml, box2d, and imgui-sfml in cmake
    - enable spdlog for logging with adjustable log levels
    - adjust runtime linking for production builds with optimized settings
    - replace std::cout with spdlog for better logging
    
    * feat(build): update cmake config for visual studio builds

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,52 @@
+#define STB_IMAGE_IMPLEMENTATION
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#include "imgui-SFML.h"
+#include "imgui.h"
+#include "spdlog/spdlog.h"
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+    // Set log level with cmake variable at compile time
+    spdlog::set_level(spdlog::level::LOG_LEVEL);
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Plat Engine", sf::Style::Default);
+
+    if(ImGui::SFML::Init(window)) {
+        spdlog::info("ImGui-SFML initialized");
+    }
+
+    float sliderValue = 0.5f;
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>())
+            {
+                spdlog::info("Window close event received");
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, sf::seconds(1.0f/60.0f));
+
+        ImGui::Begin("Demo Window", nullptr,  ImGuiWindowFlags_AlwaysAutoResize);
+        ImGui::Text("This is a demo window.");
+        ImGui::Button("Click Me");
+        ImGui::SliderFloat("Slider", &sliderValue, 0.0f, 1.0f);
+        ImGui::Text("Slider Value: %.2f", sliderValue);
+        ImGui::End();
+
+        window.clear();
+
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+    
+    ImGui::SFML::Shutdown();
+    return 0;
 }
