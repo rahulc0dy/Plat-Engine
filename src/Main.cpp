182b5b11e832e5049cd4e28831f87b504256525a
rahulc0dy <rahulchakraborty337@gmail.com>
2025-07-09 14:33:09 +0530

    feat(build): update cmake config for visual studio builds

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,29 +8,37 @@
 #include "spdlog/spdlog.h"
 
 int main() {
-
+    // Set log level with cmake variable at compile time
     spdlog::set_level(spdlog::level::LOG_LEVEL);
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Plat Engine", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Plat Engine", sf::Style::Default);
 
     if(ImGui::SFML::Init(window)) {
         spdlog::info("ImGui-SFML initialized");
     }
 
-
+    float sliderValue = 0.5f;
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);
+
             if (event->is<sf::Event::Closed>())
+            {
+                spdlog::info("Window close event received");
                 window.close();
+            }
         }
 
         ImGui::SFML::Update(window, sf::seconds(1.0f/60.0f));
 
-        ImGui::Begin("Demo Window");
+        ImGui::Begin("Demo Window", nullptr,  ImGuiWindowFlags_AlwaysAutoResize);
         ImGui::Text("This is a demo window.");
+        ImGui::Button("Click Me");
+        ImGui::SliderFloat("Slider", &sliderValue, 0.0f, 1.0f);
+        ImGui::Text("Slider Value: %.2f", sliderValue);
         ImGui::End();
 
         window.clear();
@@ -38,7 +46,7 @@ int main() {
         ImGui::SFML::Render(window);
         window.display();
     }
-
+    
     ImGui::SFML::Shutdown();
     return 0;
 }
