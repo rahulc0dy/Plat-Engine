f31070f7271a30f95fa07aa5be5d3eb22ba6ba03
Saptak Manna <saptakmanna100@gmail.com>
2025-08-16 20:56:15 +0530

    feat(logger): implement SpdLogger for enhanced logging functionality

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <memory>
+
+#include "logger/SpdlogLogger.hpp"
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    using namespace PlatLogger;
+
+    // Game/engine only knows about Logger interface
+    std::unique_ptr<Logger> logger = std::make_unique<SpdLogger>("Game");
+
+    logger->info("Game started!");
+    logger->debug("Loading assets...");
+    logger->warn("Low memory warning");
+    logger->error("Failed to load texture");
+    logger->critical("Fatal error, shutting down!");
+
     return 0;
 }
