8829b5ae07923719e07ad33b04d1e9426fc74433
Mr. 17 <saptakmanna100@gmail.com>
2025-08-17 12:09:45 +0530

    Setup spdlog globally (#25)
    * feat(logger): add Logger and LoggerFactory classes
    
    * feat(logger): remove Logger and LoggerFactory headers from CMakeLists
    
    * feat(logger): implement SpdLogger for enhanced logging functionality
    
    * feat(logging): use spdlog as public logger
    
    ---------
    
    Co-authored-by: rahulc0dy <rahulchakraborty337@gmail.com>

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "spdlog/spdlog.h"
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::warn("This is ultimate {}", "Logging");
+    spdlog::error("This is ultimate {}", "Logging");
+    spdlog::info("This is ultimate {}", "Logging");
+    spdlog::debug("This is ultimate {}", "Logging");
+    spdlog::critical("This is ultimate {}", "Logging");
     return 0;
 }
